5b5b8bd0ca6653e178f8a5cde2ea49d83d330af9
Matheus Francis <matheusvf@id.uff.br>
2025-06-07 20:55:33 -0300

    fileReader prototype created

--- projeto/src/Face.java
+++ projeto/src/Face.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Face {
-    List<Aresta> arestas;
-    Ponto normal;
+    private String nome;
+    private List<Aresta> arestas;
+    private List<Vertice> vertices;
+    private Ponto normal;
+
+    public Face(String nome) {
+
+        this.nome = nome;
+        this.arestas = new ArrayList<>();
+        this.vertices = new ArrayList<>();
+
+    }
+
+    public void addAresta(Aresta aresta) {
+
+        arestas.add(aresta);
+
+
+    }
+
+    public void addVertice(Vertice vertice) {
+
+        vertices.add(vertice);
+
+    }
+
+    public String getNome(){
+            return nome;
+
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+
+    }
 }
--- projeto/src/Vertice.java
+++ projeto/src/Vertice.java
@@ -1,7 +1,49 @@
 public class Vertice {
-    Ponto ponto;
 
-    Vertice(Ponto ponto) {
-        this.ponto = ponto;
+    private char id;
+    private double x;
+    private double y;
+    private double z;
+
+    public Vertice(char id, double x, double y, double z) {
+
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+
+    public char getId() {
+        return id;
+    }
+
+    public void setId(char id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
     }
 }
+
